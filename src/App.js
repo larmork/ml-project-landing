002a62d00612a0c296d09d6a6ff00d6c01ce911a
larmork <larmorkk@mail.ru>
2022-09-25 14:30:24 +0300

    my_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import Postitem from "./components/PostItem";
+import PostList from "./components/PostsList";
+import "./styles/App.css";
+import MyButton from "./components/UI/Button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Go Lang', body: 'Description'},
+    {id: 3, title: 'C++', body: 'Description'}
+  ])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const addNewPost = (e) => {
+    e.preventDefault()
+    const newPost = () => {
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        type="text" 
+        placeholder="Post name"/>
+        <MyInput
+        value={body}
+        onChange={e => setBody(e.target.value)}
+        type="text" 
+        placeholder="Post description"/>
+        <MyButton onClick={addNewPost}> Create post </MyButton>
+      </form>
+      <PostList posts={posts} title="Post list"/>
     </div>
   );
 }
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Postitem from "./PostItem";
+
+const PostList = ({posts, title}) => {
+
+    return (
+        <div>
+            <h1 style={{textAlign: 'center'}}>{title}</h1>
+      {posts.map(post =>
+        <Postitem post={post} key={post.id}/>
+        )}
+        </div>
+    );
+}
+
+export default PostList;
